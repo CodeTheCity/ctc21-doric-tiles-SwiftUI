9c732f7bc3ac72099776ff4c801d7562f560c7ce
Andrew Sage <andrew@asae.co.uk>
2020-11-28 12:36:05 +0000

    Initial Commit

---
+++ DoricTiles/DoricTilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoricTilesApp.swift
+//  DoricTiles
+//
+//  Created by Andrew Sage on 28/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct DoricTilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
